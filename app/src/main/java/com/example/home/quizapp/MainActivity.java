305be031a4adfe1127992bd73a4335801455a3fb
PriyaApps <hi2dharsha@gmail.com>
2017-12-09 19:21:44 -0500

    Made changes to layout and java file

--- app/src/main/java/com/example/home/quizapp/MainActivity.java
+++ app/src/main/java/com/example/home/quizapp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.home.quizapp;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.InputType;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -19,7 +15,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int score = 0;
+    int score;
+    String participantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +38,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         };
 
-        EditText name = (EditText) findViewById(R.id.name);
+        EditText name = findViewById(R.id.name);
         name.setOnClickListener(this);
         name.setOnEditorActionListener(editTextListener);
 
-        EditText qtn4 = (EditText) findViewById(R.id.qtn4_ans);
+        EditText qtn4 = findViewById(R.id.qtn4_ans);
         qtn4.setOnClickListener(this);
         qtn4.setOnEditorActionListener(editTextListener);
 
-        EditText qtn5_ans1 = (EditText) findViewById(R.id.qtn5_ans1);
+        EditText qtn5_ans1 = findViewById(R.id.qtn5_ans1);
         qtn5_ans1.setOnClickListener(this);
         qtn5_ans1.setOnEditorActionListener(editTextListener);
 
-        EditText qtn5_ans2 = (EditText) findViewById(R.id.qtn5_ans2);
+        EditText qtn5_ans2 = findViewById(R.id.qtn5_ans2);
         qtn5_ans2.setOnClickListener(this);
         qtn5_ans2.setOnEditorActionListener(editTextListener);
 
-        EditText qtn9_ans1 = (EditText) findViewById(R.id.qtn9_ans1);
+        EditText qtn9_ans1 = findViewById(R.id.qtn9_ans1);
         qtn9_ans1.setOnClickListener(this);
         qtn9_ans1.setOnEditorActionListener(editTextListener);
 
-        EditText qtn9_ans2 = (EditText) findViewById(R.id.qtn9_ans2);
+        EditText qtn9_ans2 = findViewById(R.id.qtn9_ans2);
         qtn9_ans2.setOnClickListener(this);
         qtn9_ans2.setOnEditorActionListener(editTextListener);
 
@@ -74,9 +71,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void takeQuiz(View view) {
 
-        findViewById(R.id.name).setVisibility(View.GONE);
-        findViewById(R.id.take_quiz).setVisibility(View.GONE);
-        findViewById(R.id.questions_id).setVisibility(View.VISIBLE);
+        int nameLength = ((EditText) findViewById(R.id.name)).getText().toString().length();
+        if (nameLength == 0)
+            Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show();
+        else {
+
+            participantName = ((EditText) findViewById(R.id.name)).getText().toString();
+            findViewById(R.id.name).setVisibility(View.GONE);
+            findViewById(R.id.take_quiz).setVisibility(View.GONE);
+            findViewById(R.id.questions_id).setVisibility(View.VISIBLE);
+        }
     }
 
 
@@ -88,23 +92,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void viewScore(View view) {
         findViewById(R.id.questions_id).setVisibility(View.GONE);
         findViewById(R.id.submit_button).setVisibility(View.GONE);
-        calculateScore(view);
+        calculateScore();
         displayScore();
 
     }
 
     /**
      * Calculates the score obtained by the user
-     *
-     * @param view View containing the answers given by the user
      */
 
-    private void calculateScore(View view) {
+    private void calculateScore() {
 
         //Validates the answer for the question 1
         //Adds 1 point to the score only if the correct radio button is selected
 
-        RadioGroup radioQuestion = (RadioGroup) findViewById(R.id.question_1);
+        RadioGroup radioQuestion = findViewById(R.id.question_1);
 
         if (radioQuestion.getCheckedRadioButtonId() == R.id.qtn1_ans4)
             score++;
@@ -112,22 +114,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Validates the answer for the question 2
         //Adds 1 point to the score only if the correct radio button is selected
 
-        radioQuestion = (RadioGroup) findViewById(R.id.question_2);
+        radioQuestion = findViewById(R.id.question_2);
         if (radioQuestion.getCheckedRadioButtonId() == R.id.qtn2_ans4)
             score++;
 
         //Validates the answer for the question 3
         //Adds 1 point to the score only if all of the correct checkboxes are checked and the wrong checkboxes are unchecked
 
-        CheckBox option = (CheckBox) findViewById(R.id.qtn3_ans1);
+        CheckBox option = findViewById(R.id.qtn3_ans1);
         if (option.isChecked()) {
-            option = (CheckBox) findViewById(R.id.qtn3_ans2);
+            option = findViewById(R.id.qtn3_ans2);
 
             if (option.isChecked()) {
-                option = (CheckBox) findViewById(R.id.qtn3_ans3);
+                option = findViewById(R.id.qtn3_ans3);
 
                 if (option.isChecked()) {
-                    option = (CheckBox) findViewById(R.id.qtn3_ans4);
+                    option = findViewById(R.id.qtn3_ans4);
 
                     if (!option.isChecked()) {
                         score++;
@@ -169,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Validates the answer for the question 6
         //Adds 1 point to the score only if the correct radio button is selected
 
-        radioQuestion = (RadioGroup) findViewById(R.id.question_6);
+        radioQuestion = findViewById(R.id.question_6);
 
         if (radioQuestion.getCheckedRadioButtonId() == R.id.qtn6_ans1)
             score++;
@@ -177,22 +179,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Validates the answer for the question 7
         //Adds 1 point to the score only if the correct radio button is selected
 
-        radioQuestion = (RadioGroup) findViewById(R.id.question_7);
+        radioQuestion = findViewById(R.id.question_7);
         if (radioQuestion.getCheckedRadioButtonId() == R.id.qtn7_ans3)
             score++;
 
         //Validates the answer for the question 8
         //Adds 1 point to the score only if all of the correct checkboxes are checked and the wrong checkboxes are unchecked
 
-        option = (CheckBox) findViewById(R.id.qtn8_ans1);
+        option = findViewById(R.id.qtn8_ans1);
         if (option.isChecked()) {
-            option = (CheckBox) findViewById(R.id.qtn8_ans2);
+            option = findViewById(R.id.qtn8_ans2);
 
             if (option.isChecked()) {
-                option = (CheckBox) findViewById(R.id.qtn8_ans3);
+                option = findViewById(R.id.qtn8_ans3);
 
                 if (!option.isChecked()) {
-                    option = (CheckBox) findViewById(R.id.qtn8_ans4);
+                    option = findViewById(R.id.qtn8_ans4);
 
                     if (!option.isChecked())
                         score++;
@@ -223,15 +225,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Validates the answer for the question 10
         //Adds 1 point to the score only if all of the correct checkboxes are checked and the wrong checkboxes are unchecked
 
-        option = (CheckBox) findViewById(R.id.qtn10_ans1);
+        option = findViewById(R.id.qtn10_ans1);
         if (!option.isChecked()) {
-            option = (CheckBox) findViewById(R.id.qtn10_ans2);
+            option = findViewById(R.id.qtn10_ans2);
 
             if (!option.isChecked()) {
-                option = (CheckBox) findViewById(R.id.qtn10_ans3);
+                option = findViewById(R.id.qtn10_ans3);
 
                 if (option.isChecked()) {
-                    option = (CheckBox) findViewById(R.id.qtn10_ans4);
+                    option = findViewById(R.id.qtn10_ans4);
 
                     if (option.isChecked())
                         score++;
@@ -249,7 +251,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void displayScore() {
 
-        String participantName = ((EditText) findViewById(R.id.name)).getText().toString();
         Toast toast = Toast.makeText(this, "Hello " + participantName + ",\nYour Score is " + score + ".", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_VERTICAL, 0, 0);
         toast.show();
